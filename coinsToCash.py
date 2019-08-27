19da23cecf9f306b8dbce7ba8090371cdc437b90
Mary West <westmarye1219@gmail.com>
2019-08-27 14:56:57 -0500

    wip

---
+++ coinsToCash.py
@@ -0,0 +1,28 @@
+# Activities for kids
+
+# swinging_kids = ["Marybeth", "Jenna", "Kevin", "Courtney"]
+# sliding_kids = ["Mike", "Jack", "Jennifer", "Earl"]
+# hiding_kids = ["Henry", "Heather", "Hayley", "Hugh"]
+
+
+# def running_kids(names):
+#      for name in names:
+#        print("Hello",{name})
+
+
+# for i in running_kids.values:
+#   print(i)
+
+
+def run( running_kids ):
+   running_kids = ["Pam", "Sam", "Andrea", "Will"]
+
+   print ("Values inside the function: ", mylist)
+   return
+
+def calc_dollars()
+piggyBank = {
+    "pennies": 342,
+    "nickels": 9,
+    "dimes": 32
+}
